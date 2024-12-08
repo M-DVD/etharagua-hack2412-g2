e75f0a8dee6572455e9fd26e8a5506e0ee8889d8
David Mejías <davidmejias2809@gmail.com>
2024-12-08 14:10:54 -0400

    Actualizacion a la Página Principal y creacion principal de secciones

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -11,46 +11,46 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10">
+      <div className="flex flex-col flex-grow items-center pt-10">
         <div className="px-5">
           <h1 className="text-center">
-            <span className="block text-2xl mb-2">Bienvenido </span>
+            <span className="block mb-2 text-2xl">Bienvenido </span>
             <span className="block text-4xl font-bold">Fondos De Prestaciones Empresariales Descentralizado</span>
           </h1>
-          <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
+          <div className="flex flex-col justify-center items-center space-x-2 sm:flex-row">
             <p className="my-2 font-medium">Connected Address:</p>
             <Address address={connectedAddress} />
           </div>
-          <p className="text-center text-lg">
+          <p className="text-lg text-center">
             Seguridad social descentralizada, asegurando tu futuro con transparencia y total confiabilidad !Tu fondo, tu
             tranquilidad!{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block"></code>
+            <code className="inline-block max-w-full text-base italic font-bold break-words break-all bg-base-300"></code>
           </p>
-          <p className="text-center text-lg">
+          <p className="text-lg text-center">
             {" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block"></code>{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block"></code>
+            <code className="inline-block max-w-full text-base italic font-bold break-words break-all bg-base-300"></code>{" "}
+            <code className="inline-block max-w-full text-base italic font-bold break-words break-all bg-base-300"></code>
           </p>
         </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
+        <div className="flex-grow px-8 py-12 mt-16 w-full bg-base-300">
+          <div className="flex flex-col gap-12 justify-center items-center sm:flex-row">
+            <div className="flex flex-col items-center px-10 py-10 max-w-xs text-center rounded-3xl bg-base-100">
+              <BugAntIcon className="w-8 h-8 fill-secondary" />
               <p>
                 {" "}
-                <Link href="/debug" passHref className="link">
-                  Empresa
+                <Link href="/trabajadores" passHref className="link">
+                  Zona de Empresa
                 </Link>{" "}
                 tab.
               </p>
             </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
+            <div className="flex flex-col items-center px-10 py-10 max-w-xs text-center rounded-3xl bg-base-100">
+              <MagnifyingGlassIcon className="w-8 h-8 fill-secondary" />
               <p>
                 {" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Trabajador
+                <Link href="/solicitudes" passHref className="link">
+                  Zona de Trabajador
                 </Link>{" "}
                 tab.
               </p>
