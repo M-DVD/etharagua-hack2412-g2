ad49d9e10888aef7faeceed1d457ca993435514d
Your Name <you@example.com>
2024-12-08 13:34:37 -0400

    Subo la primera modificación de la interfaz

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -15,26 +15,26 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">Fondos De Prestaciones Empresariales Descentralizado</span>
           </h1>
           <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
             <p className="my-2 font-medium">Connected Address:</p>
             <Address address={connectedAddress} />
           </div>
           <p className="text-center text-lg">
-            Get started by editing{" "}
+          Seguridad social descentralizada, asegurando tu futuro  con transparencia y total confiabilidad !Tu fondo, tu tranquilidad!{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
+              
             </code>
           </p>
           <p className="text-center text-lg">
-            Edit your smart contract{" "}
+            {" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
+              
             </code>{" "}
-            in{" "}
+            {" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
+             
             </code>
           </p>
         </div>
@@ -44,7 +44,7 @@ const Home: NextPage = () => {
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <BugAntIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Tinker with your smart contract using the{" "}
+                {" "}
                 <Link href="/debug" passHref className="link">
                   Debug Contracts
                 </Link>{" "}
@@ -54,7 +54,7 @@ const Home: NextPage = () => {
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Explore your local transactions with the{" "}
+                {" "}
                 <Link href="/blockexplorer" passHref className="link">
                   Block Explorer
                 </Link>{" "}
