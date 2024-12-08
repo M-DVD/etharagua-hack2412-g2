df7908e8cb0b03f2a9eb28dfe0a2b0481eec14cd
David Mejías <davidmejias2809@gmail.com>
2024-12-08 17:15:07 -0400

    Cambios varios, Contrato, Logo y Vista

--- packages/nextjs/app/solicitudes/page.tsx
+++ packages/nextjs/app/solicitudes/page.tsx
@@ -4,15 +4,43 @@ import { useEffect, useState } from "react";
 import { PaginationButton, SearchBar, TransactionsTable } from "./_components";
 import type { NextPage } from "next";
 import { hardhat } from "viem/chains";
-import { useFetchBlocks } from "~~/hooks/scaffold-eth";
+import { useFetchBlocks, useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
 import { notification } from "~~/utils/scaffold-eth";
+import { useAccount } from "wagmi";
+
+/*
+const verValores = (address_trabajador: any) => {
+  const { data } = useScaffoldReadContract({
+    contractName: "Prestaciones",
+    functionName: "solicitudesRetiro",
+    args: [address_trabajador],
+  });
+  return data;
+}
+*/
 
 const BlockExplorer: NextPage = () => {
   const { blocks, transactionReceipts, currentPage, totalBlocks, setCurrentPage, error } = useFetchBlocks();
   const { targetNetwork } = useTargetNetwork();
   const [isLocalNetwork, setIsLocalNetwork] = useState(true);
   const [hasError, setHasError] = useState(false);
+
+  // const [listaSolicitudes, setListaSolicitudes] = useState<any>(null);
+  const {address} = useAccount();
+
+  const { data } = useScaffoldReadContract({
+    contractName: "Prestaciones",
+    functionName: "solicitudesRetiro",
+    args: [address],
+  });
+
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+    }
+  }, [data]);
+
 
   useEffect(() => {
     if (targetNetwork.id !== hardhat.id) {
@@ -30,12 +58,12 @@ const BlockExplorer: NextPage = () => {
     if (!isLocalNetwork) {
       notification.error(
         <>
-          <p className="font-bold mt-0 mb-1">
-            <code className="italic bg-base-300 text-base font-bold"> targetNetwork </code> is not localhost
+          <p className="mt-0 mb-1 font-bold">
+            <code className="text-base italic font-bold bg-base-300"> targetNetwork </code> is not localhost
           </p>
           <p className="m-0">
-            - You are on <code className="italic bg-base-300 text-base font-bold">{targetNetwork.name}</code> .This
-            block explorer is only for <code className="italic bg-base-300 text-base font-bold">localhost</code>.
+            - You are on <code className="text-base italic font-bold bg-base-300">{targetNetwork.name}</code> .This
+            block explorer is only for <code className="text-base italic font-bold bg-base-300">localhost</code>.
           </p>
           <p className="mt-1 break-normal">
             - You can use{" "}
@@ -58,13 +86,13 @@ const BlockExplorer: NextPage = () => {
     if (hasError) {
       notification.error(
         <>
-          <p className="font-bold mt-0 mb-1">Cannot connect to local provider</p>
+          <p className="mt-0 mb-1 font-bold">Cannot connect to local provider</p>
           <p className="m-0">
-            - Did you forget to run <code className="italic bg-base-300 text-base font-bold">yarn chain</code> ?
+            - Did you forget to run <code className="text-base italic font-bold bg-base-300">yarn chain</code> ?
           </p>
           <p className="mt-1 break-normal">
-            - Or you can change <code className="italic bg-base-300 text-base font-bold">targetNetwork</code> in{" "}
-            <code className="italic bg-base-300 text-base font-bold">scaffold.config.ts</code>
+            - Or you can change <code className="text-base italic font-bold bg-base-300">targetNetwork</code> in{" "}
+            <code className="text-base italic font-bold bg-base-300">scaffold.config.ts</code>
           </p>
         </>,
       );
@@ -73,6 +101,7 @@ const BlockExplorer: NextPage = () => {
 
   return (
     <div className="container mx-auto my-10">
+      {data?.[0]}
       <SearchBar />
       <TransactionsTable blocks={blocks} transactionReceipts={transactionReceipts} />
       <PaginationButton currentPage={currentPage} totalItems={Number(totalBlocks)} setCurrentPage={setCurrentPage} />
